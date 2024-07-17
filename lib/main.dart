89c0dfacb683b328b0b8205d32690433ed73019f
KuBu250 <kubu250@gmail.com>
2024-07-17 21:51:39 +0200

    First update

--- lib/main.dart
+++ lib/main.dart
@@ -65,59 +65,20 @@ class FilledButtonApp extends StatelessWidget {
           colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
       home: Scaffold(
         appBar: AppBar(
-          title: const Center(child: Text('Piano App - KuBu250')),
+          // title: const Center(child: Text('Piano App - KuBu250')),
+          title: const Center(child: FittedBox(child: Text.rich(TextSpan(text: '  Piano App\n',
+            style: TextStyle(fontSize: 20, color: Colors.redAccent, fontWeight: FontWeight.bold),
+            children: [TextSpan(text: ' Made by KuBu250', style: TextStyle(fontStyle: FontStyle.italic, fontSize: 13, color: Colors.yellow, fontWeight: FontWeight.normal,))])),
+          )),
           backgroundColor: Colors.brown,
         ),
-        body: Center(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Column(children: <Widget>[
-                const SizedBox(height: 25),
-                FilledButton(
-                  style: const ButtonStyle(
-                      enableFeedback: false,
-                      alignment: Alignment.center,
-                      minimumSize: WidgetStatePropertyAll(Size(80, 225)),
-                      backgroundColor: WidgetStatePropertyAll(Colors.black),
-                      foregroundColor: WidgetStatePropertyAll(Colors.white)),
-                  onPressed: () {
-                    playSoundDO();
-                  },
-                  child: const Text('Do'),
-                ),
-                const SizedBox(height: 30),
-              ]),
-              const SizedBox(width: 5),
-              Column(children: <Widget>[
-                const SizedBox(height: 25),
-                FilledButton.tonal(
-                  style: ButtonStyle(
-                      enableFeedback: false,
-                      alignment: Alignment.center,
-                      minimumSize: const WidgetStatePropertyAll(Size(80, 225)),
-                      backgroundColor:
-                      const WidgetStatePropertyAll(Colors.grey),
-                      foregroundColor:
-                      const WidgetStatePropertyAll(Colors.white),
-                      side: WidgetStateProperty.all<BorderSide>(
-                        const BorderSide(
-                          width: 2.0,
-                          color: Colors
-                              .black, // Set your desired border color here
-                        ),
-                      )),
-                  onPressed: () {
-                    playSoundRE();
-                  },
-                  child: const Text('Re'),
-                ),
-                const SizedBox(height: 30),
-              ]),
-              const SizedBox(width: 5),
-              Column(children: <Widget>[
-                const SizedBox(height: 25),
-                FilledButton.tonal(
+        body: Center(child: FittedBox(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Column(children: <Widget>[
+                  const SizedBox(height: 25),
+                  FilledButton(
                     style: const ButtonStyle(
                         enableFeedback: false,
                         alignment: Alignment.center,
@@ -125,123 +86,168 @@ class FilledButtonApp extends StatelessWidget {
                         backgroundColor: WidgetStatePropertyAll(Colors.black),
                         foregroundColor: WidgetStatePropertyAll(Colors.white)),
                     onPressed: () {
-                      playSoundMI();
+                      playSoundDO();
                     },
-                    child: const Text('Mi')),
-                const SizedBox(height: 30),
-              ]),
-              const SizedBox(width: 5),
-              Column(children: <Widget>[
-                const SizedBox(height: 25),
-                FilledButton.tonal(
-                  style: ButtonStyle(
-                      enableFeedback: false,
-                      alignment: Alignment.center,
-                      minimumSize: const WidgetStatePropertyAll(Size(80, 225)),
-                      backgroundColor:
-                      const WidgetStatePropertyAll(Colors.grey),
-                      foregroundColor:
-                      const WidgetStatePropertyAll(Colors.white),
-                      side: WidgetStateProperty.all<BorderSide>(
-                        const BorderSide(
-                          width: 2.0,
-                          color: Colors
-                              .black, // Set your desired border color here
-                        ),
-                      )),
-                  onPressed: () {
-                    playSoundFA();
-                  }, child: const Text('Fa'),
-                ),
-                const SizedBox(height: 30),
-              ]),
-              const SizedBox(width: 5),
-              Column(children: <Widget>[
-                const SizedBox(height: 25),
-                FilledButton.tonal(
-                  style: const ButtonStyle(
-                      enableFeedback: false,
-                      alignment: Alignment.center,
-                      minimumSize: WidgetStatePropertyAll(Size(80, 225)),
-                      backgroundColor: WidgetStatePropertyAll(Colors.black),
-                      foregroundColor: WidgetStatePropertyAll(Colors.white)),
-                  onPressed: () {
-                    playSoundSOL();
-                  },
-                  child: const Text('Sol'),
-                ),
-                const SizedBox(height: 30),
-              ]),
-              const SizedBox(width: 5),
-              Column(children: <Widget>[
-                const SizedBox(height: 25),
-                FilledButton.tonal(
-                  style: ButtonStyle(
-                      enableFeedback: false,
-                      alignment: Alignment.center,
-                      minimumSize: const WidgetStatePropertyAll(Size(80, 225)),
-                      backgroundColor:
-                      const WidgetStatePropertyAll(Colors.grey),
-                      foregroundColor:
-                      const WidgetStatePropertyAll(Colors.white),
-                      side: WidgetStateProperty.all<BorderSide>(
-                        const BorderSide(
-                          width: 2.0,
-                          color: Colors
-                              .black, // Set your desired border color here
-                        ),
-                      )),
-                  onPressed: () {
-                    playSoundLA();
-                  },
-                  child: const Text('La'),
-                ),
-                const SizedBox(height: 30),
-              ]),
-              const SizedBox(width: 5),
-              Column(children: <Widget>[
-                const SizedBox(height: 25),
-                FilledButton.tonal(
-                  style: const ButtonStyle(
-                      enableFeedback: false,
-                      alignment: Alignment.center,
-                      minimumSize: WidgetStatePropertyAll(Size(80, 225)),
-                      backgroundColor: WidgetStatePropertyAll(Colors.black),
-                      foregroundColor: WidgetStatePropertyAll(Colors.white)),
-                  onPressed: () {
-                    playSoundSI();
-                  },
-                  child: const Text('Si'),
-                ),
-                const SizedBox(height: 30),
-              ]),
-              const SizedBox(width: 5),
-              Column(children: <Widget>[
-                const SizedBox(height: 25),
-                FilledButton(
-                  style: ButtonStyle(
-                      enableFeedback: false,
-                      alignment: Alignment.center,
-                      minimumSize: const WidgetStatePropertyAll(Size(80, 225)),
-                      backgroundColor:
-                      const WidgetStatePropertyAll(Colors.grey),
-                      foregroundColor:
-                      const WidgetStatePropertyAll(Colors.white),
-                      side: WidgetStateProperty.all<BorderSide>(
-                        const BorderSide(
-                          width: 2.0,
-                          color: Colors
-                              .black, // Set your desired border color here
-                        ),
-                      )),
-                  onPressed: () {
-                    playSoundDO2();
-                  },
-                  child: const Text('Do'),
-                ),
-                const SizedBox(height: 30),
-              ]),
-            ],
+                    child: const Text('Do'),
+                  ),
+                  const SizedBox(height: 30),
+
+                ]),
+                const SizedBox(width: 5),
+                Column(children: <Widget>[
+                  const SizedBox(height: 25),
+                  FilledButton.tonal(
+                    style: ButtonStyle(
+                        enableFeedback: false,
+                        alignment: Alignment.center,
+                        minimumSize: const WidgetStatePropertyAll(Size(80, 225)),
+                        backgroundColor:
+                        const WidgetStatePropertyAll(Colors.grey),
+                        foregroundColor:
+                        const WidgetStatePropertyAll(Colors.white),
+                        side: WidgetStateProperty.all<BorderSide>(
+                          const BorderSide(
+                            width: 2.0,
+                            color: Colors
+                                .black, // Set your desired border color here
+                          ),
+                        )),
+                    onPressed: () {
+                      playSoundRE();
+                    },
+                    child: const Text('Re'),
+                  ),
+                  const SizedBox(height: 30),
+                ]),
+                const SizedBox(width: 5),
+                Column(children: <Widget>[
+                  const SizedBox(height: 25),
+                  FilledButton.tonal(
+                      style: const ButtonStyle(
+                          enableFeedback: false,
+                          alignment: Alignment.center,
+                          minimumSize: WidgetStatePropertyAll(Size(80, 225)),
+                          backgroundColor: WidgetStatePropertyAll(Colors.black),
+                          foregroundColor: WidgetStatePropertyAll(Colors.white)),
+                      onPressed: () {
+                        playSoundMI();
+                      },
+                      child: const Text('Mi')),
+                  const SizedBox(height: 30),
+                ]),
+                const SizedBox(width: 5),
+                Column(children: <Widget>[
+                  const SizedBox(height: 25),
+                  FilledButton.tonal(
+                    style: ButtonStyle(
+                        enableFeedback: false,
+                        alignment: Alignment.center,
+                        minimumSize: const WidgetStatePropertyAll(Size(80, 225)),
+                        backgroundColor:
+                        const WidgetStatePropertyAll(Colors.grey),
+                        foregroundColor:
+                        const WidgetStatePropertyAll(Colors.white),
+                        side: WidgetStateProperty.all<BorderSide>(
+                          const BorderSide(
+                            width: 2.0,
+                            color: Colors
+                                .black, // Set your desired border color here
+                          ),
+                        )),
+                    onPressed: () {
+                      playSoundFA();
+                    }, child: const Text('Fa'),
+                  ),
+                  const SizedBox(height: 30),
+                ]),
+                const SizedBox(width: 5),
+                Column(children: <Widget>[
+                  const SizedBox(height: 25),
+                  FilledButton.tonal(
+                    style: const ButtonStyle(
+                        enableFeedback: false,
+                        alignment: Alignment.center,
+                        minimumSize: WidgetStatePropertyAll(Size(80, 225)),
+                        backgroundColor: WidgetStatePropertyAll(Colors.black),
+                        foregroundColor: WidgetStatePropertyAll(Colors.white)),
+                    onPressed: () {
+                      playSoundSOL();
+                    },
+                    child: const Text('Sol'),
+                  ),
+                  const SizedBox(height: 30),
+                ]),
+                const SizedBox(width: 5),
+                Column(children: <Widget>[
+                  const SizedBox(height: 25),
+                  FilledButton.tonal(
+                    style: ButtonStyle(
+                        enableFeedback: false,
+                        alignment: Alignment.center,
+                        minimumSize: const WidgetStatePropertyAll(Size(80, 225)),
+                        backgroundColor:
+                        const WidgetStatePropertyAll(Colors.grey),
+                        foregroundColor:
+                        const WidgetStatePropertyAll(Colors.white),
+                        side: WidgetStateProperty.all<BorderSide>(
+                          const BorderSide(
+                            width: 2.0,
+                            color: Colors
+                                .black, // Set your desired border color here
+                          ),
+                        )),
+                    onPressed: () {
+                      playSoundLA();
+                    },
+                    child: const Text('La'),
+                  ),
+                  const SizedBox(height: 30),
+                ]),
+                const SizedBox(width: 5),
+                Column(children: <Widget>[
+                  const SizedBox(height: 25),
+                  FilledButton.tonal(
+                    style: const ButtonStyle(
+                        enableFeedback: false,
+                        alignment: Alignment.center,
+                        minimumSize: WidgetStatePropertyAll(Size(80, 225)),
+                        backgroundColor: WidgetStatePropertyAll(Colors.black),
+                        foregroundColor: WidgetStatePropertyAll(Colors.white)),
+                    onPressed: () {
+                      playSoundSI();
+                    },
+                    child: const Text('Si'),
+                  ),
+                  const SizedBox(height: 30),
+                ]),
+                const SizedBox(width: 5),
+                Column(children: <Widget>[
+                  const SizedBox(height: 25),
+                  FilledButton(
+                    style: ButtonStyle(
+                        enableFeedback: false,
+                        alignment: Alignment.center,
+                        minimumSize: const WidgetStatePropertyAll(Size(80, 225)),
+                        backgroundColor:
+                        const WidgetStatePropertyAll(Colors.grey),
+                        foregroundColor:
+                        const WidgetStatePropertyAll(Colors.white),
+                        side: WidgetStateProperty.all<BorderSide>(
+                          const BorderSide(
+                            width: 2.0,
+                            color: Colors
+                                .black, // Set your desired border color here
+                          ),
+                        )),
+                    onPressed: () {
+                      playSoundDO2();
+                    },
+                    child: const Text('Do'),
+                  ),
+                  const SizedBox(height: 30),
+                ]),
+              ],
+            ),
           ),
         ),
       ),
